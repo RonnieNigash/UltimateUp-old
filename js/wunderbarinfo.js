1e4bc8dbf50cfe390dcb912e7551c1f0f6f915b0
hotwinter <xiaoba2008@gmail.com>
2015-02-22 11:40:35 -0800

    wunderbarinfo logged to console

--- js/wunderbarinfo.js
+++ js/wunderbarinfo.js
@@ -1,10 +1,10 @@
 var relayr = RELAYR.init({
-  appId: ""
+  appId: "c5eac84a-1511-4cda-8136-9af661257cf7"
 });
 
 relayr.devices().getDeviceData({
-  deviceId: "",
-  token: token,
+  deviceId: "c4e8751d-f9ff-4509-ba0c-98b78afb0576",
+  token: "_C4efSMSUjtisJQybuiC5PN_pkVW.P-S" ,
   incomingData: function(data){
     console.log("sensor",data);
   }
